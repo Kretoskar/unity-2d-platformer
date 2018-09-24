1bd09fe6e56917a31ff1044aa309926d0d4fa70a
Oskar Kogut <kogutoskar123@gmail.com>
2018-09-25 01:37:31 +0200

    Added singleton pattern and 2nd level

---
+++ platformer/Assets/classes/Teleport.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Teleport : MonoBehaviour {
+
+    [SerializeField] private int mSceneIndexToLoad = 1;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        SceneManager.LoadScene(mSceneIndexToLoad);
+    }
+}
