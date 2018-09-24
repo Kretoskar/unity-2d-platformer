28a6f50adc9b7eac716e7218cacff8bb7ef605ea
Oskar Kogut <kogutoskar123@gmail.com>
2018-09-25 00:45:08 +0200

    Added green coin

--- platformer/Assets/classes/LevelManager.cs
+++ platformer/Assets/classes/LevelManager.cs
@@ -8,9 +8,20 @@ public class LevelManager : MonoBehaviour {
     [SerializeField] private Text mScoreText;
 
     private int mScore = 0;
+    private int numberOfGameSession;
 
     private void Start() {
+        SingletonPattern();
         mScoreText.text = mScore.ToString();
+    }
+
+    private void SingletonPattern() {
+        numberOfGameSession = FindObjectsOfType<LevelManager>().Length;
+        if (numberOfGameSession > 1) {
+            Destroy(gameObject);
+        } else {
+            DontDestroyOnLoad(gameObject);
+        }
     }
 
     private void Update() {
